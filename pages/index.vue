72c7b4933662ba4614c5d1cc8ad7c243cd8ab6e6
Ariel Adams <arieladams@PearyTheMactypus.local>
2022-03-26 18:55:33 -0600

    feat: randomize reading prompt

--- pages/index.vue
+++ pages/index.vue
@@ -5,33 +5,36 @@
       </h1>
     </header>
     <main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+      <h2 class="block text-gray-700 text-sm font-bold mb-2 uppercase">Read a book that...</h2>
       <section>
-            <form class="flex flex-col" method="POST" action="#">
-                <button class="bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 mb-6" type="submit">Prompt Me</button>
-                <div class="pt-3 rounded bg-gray-200">
-                  <div class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 text-center">
-                    prompt prints here
-                  </div>
-                </div>
-            </form>
-        </section>
-        <div class="reading-prompts">
-          <!-- {{ readingPrompts }} -->
+        <div class="flex flex-col">
+          <div class="pt-3 rounded bg-gray-200">
+            <div class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 text-center">
+              <p>{{ prompts[promptIndex].string }}</p>
+            </div>
+          </div>
+          <button class="mt-6 bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit" v-on:click="randomize">Prompt Me</button>
         </div>
+      </section>
     </main>
   </div>
 </template>
 
 <script>
 import readingPrompts from '~/static/data/prompts.json'
+
 export default {
   name: 'Home',
-  // async asyncData({ $axios }) {
-  //   const data = await $axios.$get('/data/prompts.json')
-  //   return { data }
-  // }
-  async asyncData({ params }) {
-    return { readingPrompts }
+  data() {
+    return {
+      prompts: readingPrompts,
+      promptIndex: 0 
+    };
+  },
+  methods: {
+    randomize: function() {
+      this.promptIndex = Math.floor(Math.random() * (this.prompts.length));
+    }
   }
 }
 </script>
